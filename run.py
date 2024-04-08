8e3bea57f8c646ad4cce8e7a74bd32f5bf331575
Desislava Naydenova <deskata.01@gmail.com>
2024-04-08 08:37:58 +0000

    create tha gameboard and print it

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Create board
+board = [ "-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+
+#Printing the gameboard
+def printGameboard(board):
+    print(board[0]+ " | "+ board[1]+ " | "+ board[2])
+    print("----------")
+    print(board[3]+ " | "+ board[4]+ " | "+ board[5])
+    print("----------")
+    print(board[6]+ " | "+ board[7]+ " | "+ board[8])
+
+printGameboard(board)
+#Take player input
+
+
+#Check for Win or Tie
+
+
+#Switch player
+
+
+#Check for Win or Tie again
