2ab4a0d6e0fcff8aa270a7b29ade965fc52def39
Desislava Naydenova <deskata.01@gmail.com>
2024-04-08 08:53:46 +0000

    get input fromm the first player

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #Create board
-board = [ "-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-"]
+board = [ " -", "-", "-",
+         " -", "-", "-",
+         " -", "-", "-"]
 currentPlayer = "X"
 winner = None
 gameRunning = True
@@ -10,13 +10,20 @@ gameRunning = True
 #Printing the gameboard
 def printGameboard(board):
     print(board[0]+ " | "+ board[1]+ " | "+ board[2])
-    print("----------")
+    print("-----------")
     print(board[3]+ " | "+ board[4]+ " | "+ board[5])
-    print("----------")
+    print("-----------")
     print(board[6]+ " | "+ board[7]+ " | "+ board[8])
 
 printGameboard(board)
 #Take player input
+
+def playerInput(board):
+    inp = int(input("Enter a nummber form 1 to 9:\n"))
+    if inp >= 1 and inp <= 9 and board[inp -1]== "-":
+        board[inp -1] = currentPlayer
+playerInput(board)
+printGameboard(board)
 
 
 #Check for Win or Tie
